3f93b422f012aca290bdbd6429fa0daa53f94b45
Suélio <suelio.pontes@hotmail.com>
2017-11-22 14:15:05 -0200

    X

--- public/vagas/js/controllers/contato/contatoEditCtrl.js
+++ public/vagas/js/controllers/contato/contatoEditCtrl.js
@@ -2,9 +2,9 @@ angular.module("app").controller("contatoEditCtrl", function ($scope, $routePara
 	$scope.contato = contato.data;
 	$scope.saveContato = function (id,contato) {		
 		contatoAPI.updateContato(id,contato).success(function (data) {
-			delete $scope.contato;
-			$scope.contatoForm.$setPristine();
+			delete $scope.contato;			
 			$location.path("/contato/"+contato.user);
+			$scope.contatoForm.$setPristine();
 			
 		});
 	};

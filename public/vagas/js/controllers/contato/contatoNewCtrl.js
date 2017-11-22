c361a83798d8bc8c7cd38d9ef2a632a19d74479e
Suélio <suelio.pontes@hotmail.com>
2017-11-22 13:08:36 -0200

    X

--- public/vagas/js/controllers/contato/contatoNewCtrl.js
+++ public/vagas/js/controllers/contato/contatoNewCtrl.js
@@ -2,7 +2,8 @@ angular.module("app").controller("contatoNewCtrl", function ($scope,contatoAPI,$
    
         $scope.locatarioId=locatarioId;
         
-        $scope.adicionarContato = function (contato) {		
+        $scope.adicionarContato = function (contato) {
+            contato.user=locatarioId;		
             contatoAPI.saveContato(contato).success(function (data) {
                 delete $scope.contato;
                 $scope.contatoForm.$setPristine();

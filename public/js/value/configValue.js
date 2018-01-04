beecca569ee04f298db84939237125a59524814d
Suélio <suelio.pontes@hotmail.com>
2018-01-04 11:20:53 -0200

    Resetando a branch

---
+++ public/js/value/configValue.js
@@ -0,0 +1,7 @@
+angular.module("app").value("val", {
+	// ambiente de desenvolvimento
+	//baseUrl: "http://localhost:3000/api"
+	
+	//  ambiente de produção
+	baseUrl: "https://appnunes.herokuapp.com/api"
+});
